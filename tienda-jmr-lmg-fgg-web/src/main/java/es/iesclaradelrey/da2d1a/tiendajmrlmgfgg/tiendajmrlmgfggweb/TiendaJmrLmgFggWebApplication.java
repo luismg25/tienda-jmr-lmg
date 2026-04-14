38a7f31df3a575041a5b6c05193abac62508a317
jorge-commits-6 <jorgemarcoranilla@gmail.com>
2026-04-14 10:57:26 +0200

    Cambio a estructura hibrida con modulo web y pom padre

---
+++ tienda-jmr-lmg-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgfgg/tiendajmrlmgfggweb/TiendaJmrLmgFggWebApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendajmrlmgfgg.tiendajmrlmgfggweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJmrLmgFggWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaJmrLmgFggWebApplication.class, args);
+    }
+
+}
