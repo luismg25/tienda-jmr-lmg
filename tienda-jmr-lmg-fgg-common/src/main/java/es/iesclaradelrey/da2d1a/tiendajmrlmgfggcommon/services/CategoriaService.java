304572422222cf0e22af1e2dd5ac06601b8a80d7
jorge-commits-6 <jorgemarcoranilla@gmail.com>
2026-04-20 13:02:04 +0200

    Implementacion del servicio y escaneo de paquetes en webApp

--- tienda-jmr-lmg-fgg-common/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgfggcommon/services/CategoriaService.java
+++ tienda-jmr-lmg-fgg-common/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgfggcommon/services/CategoriaService.java
@@ -3,8 +3,10 @@ package es.iesclaradelrey.da2d1a.tiendajmrlmgfggcommon.services;
 import es.iesclaradelrey.da2d1a.tiendajmrlmgfggcommon.entities.Categoria;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoriaService {
     List<Categoria> findAll();
-
+    Optional<Categoria> findById(Long id);
+    Categoria save(Categoria categoria);
 }
