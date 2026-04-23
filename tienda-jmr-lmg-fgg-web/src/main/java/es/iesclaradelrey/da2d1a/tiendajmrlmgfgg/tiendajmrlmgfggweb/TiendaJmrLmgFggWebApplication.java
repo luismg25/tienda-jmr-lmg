75c925cfd18176ac681ed00784cc154897daef9c
jorge-commits-6 <jorgemarcoranilla@gmail.com>
2026-04-23 08:36:05 +0200

    avance en templates y correciones varias

--- tienda-jmr-lmg-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgfgg/tiendajmrlmgfggweb/TiendaJmrLmgFggWebApplication.java
+++ tienda-jmr-lmg-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgfgg/tiendajmrlmgfggweb/TiendaJmrLmgFggWebApplication.java
@@ -1,19 +1,34 @@
+//package es.iesclaradelrey.da2d1a.tiendajmrlmgfgg.tiendajmrlmgfggweb;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication (
+//scanBasePackages = {
+//        //Spring busca en commonlos repositorios, servicios y controladores.
+//        "es.iesclaradelrey.da2d1a.tiendajmrlmgfggcommon",
+//        "es.iesclaradelrey.da2d1a.tiendajmrlmgfggweb"
+//        }
+//)
+//public class TiendaJmrLmgFggWebApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(TiendaJmrLmgFggWebApplication.class, args);
+//    }
+//
+//}
 package es.iesclaradelrey.da2d1a.tiendajmrlmgfgg.tiendajmrlmgfggweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (
-scanBasePackages = {
-        //Spring busca en commonlos repositorios, servicios y controladores.
-        "es.iesclaradelrey.da2d1a.tiendajmrlmgfgg.tiendajmrlmgfggcommon",
-        "es.iesclaradelrey.da2d1a.tiendajmrlmgfgg.tiendajmrlmgfggweb"
-        }
+@SpringBootApplication(
+        // Al poner la carpeta "da2d1a", Spring buscará hacia adentro en TODAS las subcarpetas,
+        // pillando tanto el módulo web como el common sin equivocarse.
+        scanBasePackages = "es.iesclaradelrey.da2d1a"
 )
 public class TiendaJmrLmgFggWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TiendaJmrLmgFggWebApplication.class, args);
     }
-
 }
