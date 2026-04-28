2bdc785b5b51894e5291f5cfd11103f4ab185b02
jorge-commits-6 <jorgemarcoranilla@gmail.com>
2026-04-28 19:45:35 +0200

    Migrar a h2

--- tienda-jmr-lmg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgweb/WebApp.java
+++ tienda-jmr-lmg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajmrlmgweb/WebApp.java
@@ -2,9 +2,14 @@ package es.iesclaradelrey.da2d1a.tiendajmrlmgweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 // Spring escaneará en TODAS las subcarpetas, tanto de web como de common
 @SpringBootApplication(scanBasePackages = "es.iesclaradelrey.da2d1a")
+@EnableJpaRepositories(basePackages = "es.iesclaradelrey.da2d1a")//Busca los repositorios JPA
+@EntityScan(basePackages = "es.iesclaradelrey.da2d1a")//Busca las clases Entidad
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
